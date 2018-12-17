2afc8b38f16d062e39a4ac9a9ee80ac20100b27a
Melvin Philips <melvin0008@gmail.com>
2018-12-16 19:46:01 -0800

    Add Level db functions to test blockchain

--- BlockChain.js
+++ BlockChain.js
@@ -10,40 +10,85 @@ class Blockchain {
 
     constructor() {
         this.bd = new LevelSandbox.LevelSandbox();
-        this.generateGenesisBlock();
+        this.generateGenesisBlock().then(created => { if (created) console.log("Successfully created first block") });
     }
 
     // Auxiliar method to create a Genesis Block (always with height= 0)
     // You have to options, because the method will always execute when you create your blockchain
     // you will need to set this up statically or instead you can verify if the height !== 0 then you
     // will not create the genesis block
-    generateGenesisBlock(){
+    async generateGenesisBlock() {
         // Add your code here
+        let height = await this.getBlockHeight();
+        if (height !== -1) return false;
+        return await this.addBlock(new Block.Block("First block in the chain - Genesis block"));
     }
 
     // Get block height, it is auxiliar method that return the height of the blockchain
-    getBlockHeight() {
+    async getBlockHeight() {
         // Add your code here
+        return this.bd.getBlocksCount();
     }
 
     // Add new block
-    addBlock(block) {
+    async addBlock(block) {
         // Add your code here
+        const height = await this.getBlockHeight();
+        block.height = height + 1;
+        block.time = new Date().getTime().toString().slice(0,-3);
+
+        if (block.height > 0) {
+            const previousBlock = await this.getBlock(block.height - 1);
+            block.previousBlockHash = previousBlock.hash;
+        }
+        block.hash = this._generateBlockHash(block);
+        return await this.bd.addLevelDBData(block.height, JSON.stringify(block));
     }
 
     // Get Block By Height
-    getBlock(height) {
+    async getBlock(height) {
         // Add your code here
+        let blockData = await this.bd.getLevelDBData(height);
+        return JSON.parse(blockData);
     }
 
     // Validate if Block is being tampered by Block Height
-    validateBlock(height) {
+    async validateBlock(height) {
         // Add your code here
+        let block = await this.getBlock(height);
+        let blockHash = block.hash;
+        block.hash = '';
+        let validateBlockHash = this._generateBlockHash(block);
+        if (blockHash === validateBlockHash) {
+            return true;
+        } else {
+            console.log('Block #' + height + ' invalid hash:\n' + blockHash + '<>' + validateBlockHash);
+            return false;
+        }
     }
 
     // Validate Blockchain
-    validateChain() {
+    async validateChain() {
         // Add your code here
+        let errorLog = [];
+        let previousBlockHash = "";
+        let currentHeight = await this.getBlockHeight();
+        for (let height = 0; height < currentHeight; height++) {
+            if (!await this.validateBlock(height)) errorLog.push(height);
+            let block = await this.getBlock(height);
+            if (block.previousBlockHash != previousBlockHash) {
+                errorLog.push(height);
+            }
+            previousBlockHash = block.hash;
+        }
+
+        if (errorLog.length > 0) {
+            console.log('Block errors = ' + errorLog.length);
+            console.log('Blocks: ' + errorLog);
+        } else {
+            console.log('No errors detected');
+        }
+        return errorLog;
     }
 
     // Utility Method to Tamper a Block for Test Validation
@@ -55,6 +100,10 @@ class Blockchain {
                 resolve(blockModified);
             }).catch((err) => { console.log(err); reject(err)});
         });
+    }
+
+    _generateBlockHash(block) {
+        return SHA256(JSON.stringify(block)).toString();
     }
 }
 
--- LevelSandbox.js
+++ LevelSandbox.js
@@ -2,38 +2,52 @@
 |  Learn more: level: https://github.com/Level/level |
 /===================================================*/
 
-const level = require('level');
-const chainDB = './chaindata';
+    const level = require('level');
+    const chainDB = './chaindata';
 
-class LevelSandbox {
+    class LevelSandbox {
 
-    constructor() {
-        this.db = level(chainDB);
+        constructor() {
+            this.db = level(chainDB);
+        }
+
+        // Get data from levelDB with key
+        async getLevelDBData(key){
+            let val;
+            try {
+                val = await this.db.get(key);
+            } catch (err) {
+                console.log("Not found!");
+                return null;
+            }
+            return val;
+        }
+
+        // Add data to levelDB with key and value
+        async addLevelDBData(key, value) {
+            try {
+                await this.db.put(key, value);
+                return true;
+            } catch (err) {
+                console.log('Block ' + key + ' submission failed', err);
+                return false;
+            }
+        }
+
+        // Method that return the height
+        getBlocksCount() {
+            return new Promise( (resolve, reject) => {
+                let height = -1;
+                this.db.createReadStream().on('data', (data) => {
+                    height++;
+                }).on('error', (err) => {
+                    reject(err);
+                }).on('close', () => {
+                    resolve(height);
+                });
+            })
+        }
+
     }
-
-    // Get data from levelDB with key (Promise)
-    getLevelDBData(key){
-        let self = this;
-        return new Promise(function(resolve, reject) {
-            // Add your code here, remember un Promises you need to resolve() or reject()
-        });
-    }
-
-    // Add data to levelDB with key and value (Promise)
-    addLevelDBData(key, value) {
-        let self = this;
-        return new Promise(function(resolve, reject) {
-            // Add your code here, remember un Promises you need to resolve() or reject()
-        });
-    }
-
-    // Method that return the height
-    getBlocksCount() {
-        let self = this;
-        return new Promise(function(resolve, reject){
-            // Add your code here, remember un Promises you need to resolve() or reject()
-        });
-    }
-}
 
 module.exports.LevelSandbox = LevelSandbox;
